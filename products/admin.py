b2b4c68aa6d6a259b2384a07d1410d0d29e3217d
hugo <hugomvera@gmail.com>
2022-04-17 11:06:30 -0700

    ready to publish

---
+++ products/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Product, Offer
+
+
+# Register your models here.
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discounts')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
+admin.site.register(Offer, OfferAdmin)
+admin.site.register(Product, ProductAdmin)
